d268236bfbde3d2a93e7d9f0023670a15af969fb
DESKTOP-BKMP9GD\luiza <luiz.adolphs@gmail.com>
2017-08-20 21:21:29 -0300

    Work in progress

---
+++ introducao/run-windows.ps1
@@ -0,0 +1,4 @@
+$exp =  'docker build -f "'+ $PSScriptRoot +'/Dockerfile" -t workshop-docker-parte1 .;'
+$exp += 'docker run -p 8080:8080 -v "'+ $PSScriptRoot +':/app" --name introducao --rm -it -t workshop-docker-parte1;'
+
+Invoke-Expression $exp
